22b69719edfcf2fe279e50aa67aba7082ee2adc2
Kush Shah <ks4437@nyu.edu>
2016-11-13 16:30:26 -0500

    added a file

--- prog.sh
+++ prog.sh
@@ -1,7 +1,9 @@
-  GNU nano 2.2.6                                            File: download1.sh
+GNU nano 2.2.6                                            File: download1.sh
+
+#!/bin/bash
 
 for i in  1 2 3 4 5 6 7 8 9 ; do
-        #wget http://witestlab.poly.edu/bikes/20160"$i"-citibike-tripdata.zip
+        wget http://witestlab.poly.edu/bikes/20160"$i"-citibike-tripdata.zip
         unzip 20160"$i"-citibike-tripdata.zip
 done
 
